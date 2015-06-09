b5478936c8462800a172bda418d85e76261d87ab
cwnidog <cwnidog@comcast.net>
2015-06-09 14:59:21 -0700

    Adding basic code to create an array of NSDates

--- DateList/main.m
+++ DateList/main.m
@@ -8,10 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
-  }
+int main(int argc, const char *argv[])
+{
+  @autoreleasepool
+  {
+    // Create three NSDate objects
+    NSDate *now = [NSDate date];
+    NSDate *tomorrow = [now dateByAddingTimeInterval:24.0 * 60.0 * 60.0];
+    NSDate *yesterday = [now dateByAddingTimeInterval:-24.0 * 60.0 * 60.0];
+    
+    // Create an arrary containing all three
+    NSArray *dateList = @[now, tomorrow, yesterday];
+  } // autoresponsepool
     return 0;
-}
+} // main()
